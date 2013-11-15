5fa21fc5d0967c661da302f36e8114af124817f5
Sean McMains <sean@mcmains.net>
2013-11-15 10:27:37 -0600

    Pushing and displaying lat/long to watch.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,14 +1,3 @@
-function successCallback(position) {
-	console.log("Got position:");
-	console.log(position.coords.latitude);
-	console.log(position.coords.longitude);
-}
-
-function errorCallback(error) {
-	console.log("Got error:");
-	console.log(error);
-}
-
 Pebble.addEventListener("ready", function(e) {
 	console.log("Hello world! - Sent from your javascript application.");
 
@@ -20,3 +9,30 @@ Pebble.addEventListener("ready", function(e) {
 	console.log("And in theory, everything should be set up");
 });
 
+function successCallback(position) {
+	console.log("Got position:");
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	console.log(latitude);
+	console.log(longitude);
+
+	data = {
+		"1": String(latitude),
+		"2": String(longitude)
+	};
+	Pebble.sendAppMessage(data, ackHandler, nackHandler)
+}
+
+function errorCallback(error) {
+	console.log("Got error:");
+	console.log(error);
+}
+
+function ackHandler(e) {
+	console.log("Message sent to pebble successfully");
+}
+
+function nackHandler(e) {
+	console.log("Error sending message to pebble");
+	console.log(JSON.stringify(e, null, 4));
+}
--- src/my_js_project.c
+++ src/my_js_project.c
@@ -1,54 +1,69 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *latitude_layer;
+static TextLayer *longitude_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+// Handle messages
+void in_received_handler(DictionaryIterator *received, void *context) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Got message from Javascript");
+
+	Tuple *latitude_tuple = dict_find(received, 1);
+	if ( latitude_tuple ) {
+		text_layer_set_text( latitude_layer, latitude_tuple->value->cstring);
+	}
+
+	Tuple *longitude_tuple = dict_find(received, 2);
+	if ( longitude_tuple ) {
+		text_layer_set_text( longitude_layer, longitude_tuple->value->cstring);
+	}
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
+// Set everything up
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  latitude_layer = text_layer_create((GRect) { .origin = { 0, 40 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(latitude_layer, "Latitude");
+  text_layer_set_text_alignment(latitude_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(latitude_layer));
+
+  longitude_layer = text_layer_create((GRect) { .origin = { 0, 80 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(longitude_layer, "Longitude");
+  text_layer_set_text_alignment(longitude_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(longitude_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(latitude_layer);
+  text_layer_destroy(longitude_layer);
+}
+
+static void setupMessaging(void) {
+	app_message_register_inbox_received(in_received_handler);
+	const uint32_t inbound_size = 64;
+	const uint32_t outbound_size = 64;
+	app_message_open(inbound_size, outbound_size);
 }
 
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
   window_stack_push(window, animated);
+setupMessaging();
 }
 
+// Tear everything down
 static void deinit(void) {
   window_destroy(window);
 }
+
+// Main
 
 int main(void) {
   init();
