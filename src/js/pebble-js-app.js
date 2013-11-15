280b62842a6eb01bcb26d7a3f0e637bdf327df6f
Sean McMains <sean@mcmains.net>
2013-11-15 17:00:17 -0600

    Find nearby location with Foursquare API.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -6,6 +6,8 @@ Pebble.addEventListener("ready", function(e) {
 		navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 	}, 10000);
 
+	navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
 	console.log("And in theory, everything should be set up");
 });
 
@@ -16,11 +18,37 @@ function successCallback(position) {
 	console.log(latitude);
 	console.log(longitude);
 
-	data = {
-		"1": String(latitude),
-		"2": String(longitude)
-	};
-	Pebble.sendAppMessage(data, ackHandler, nackHandler)
+	getFoursquareData(latitude, longitude);
+}
+
+function getFoursquareData(latitude, longitude) {
+	var queryString = "https://api.foursquare.com/v2/venues/search?ll=" + latitude + "," + longitude + "&limit=1&&client_id=SP2DN3CY5EBLVHIBZWTXYTLVGPBILJBLJ2VGIGHHRPIT3FKH&client_secret=ALC31KIBGEXM3VVB0OMQUY15BHTD5VV4DUNFBNTT3QMG53XQ&v=20131016";
+	console.log("I'd like to ask foursquare this: " + queryString);
+
+	var req = new XMLHttpRequest();
+	req.open('GET', queryString, true);
+	req.onload = function(e) {
+		if (req.readyState == 4 && req.status == 200) {
+			if (req.status == 200) {
+				var response = JSON.parse(req.responseText);
+				console.log("Got this from Foursquare: " + JSON.stringify(response, null, 4));
+				console.log("4sq: " + response.response.venues[0].name);
+
+				data = {
+					"1": String(latitude),
+					"2": String(longitude),
+					"3": String(response.response.venues[0].name)
+				};
+				Pebble.sendAppMessage(data, ackHandler, nackHandler)
+
+			} else {
+				console.log("Error");
+			}
+		}
+	}
+	console.log("sending request to foursquare");
+	req.send(null);
+
 }
 
 function errorCallback(error) {
--- src/my_js_project.c
+++ src/my_js_project.c
@@ -3,6 +3,7 @@
 static Window *window;
 static TextLayer *latitude_layer;
 static TextLayer *longitude_layer;
+static TextLayer *nearby_layer;
 
 // Handle messages
 void in_received_handler(DictionaryIterator *received, void *context) {
@@ -10,12 +11,20 @@ void in_received_handler(DictionaryIterator *received, void *context) {
 
 	Tuple *latitude_tuple = dict_find(received, 1);
 	if ( latitude_tuple ) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "1");
 		text_layer_set_text( latitude_layer, latitude_tuple->value->cstring);
 	}
 
 	Tuple *longitude_tuple = dict_find(received, 2);
 	if ( longitude_tuple ) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "2");
 		text_layer_set_text( longitude_layer, longitude_tuple->value->cstring);
+	}
+	
+	Tuple *nearby_tuple = dict_find(received, 3);
+	if ( nearby_tuple ) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "3");
+		text_layer_set_text( nearby_layer, nearby_tuple->value->cstring);
 	}
 }
 
@@ -24,26 +33,32 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  latitude_layer = text_layer_create((GRect) { .origin = { 0, 40 }, .size = { bounds.size.w, 20 } });
+  latitude_layer = text_layer_create((GRect) { .origin = { 0, 30 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(latitude_layer, "Latitude");
   text_layer_set_text_alignment(latitude_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(latitude_layer));
 
-  longitude_layer = text_layer_create((GRect) { .origin = { 0, 80 }, .size = { bounds.size.w, 20 } });
+  longitude_layer = text_layer_create((GRect) { .origin = { 0, 50 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(longitude_layer, "Longitude");
   text_layer_set_text_alignment(longitude_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(longitude_layer));
+
+  nearby_layer = text_layer_create((GRect) { .origin = { 0, 90 }, .size = { bounds.size.w, 20 } });
+  text_layer_set_text(nearby_layer, "Nearby");
+  text_layer_set_text_alignment(nearby_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(nearby_layer));
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(latitude_layer);
   text_layer_destroy(longitude_layer);
+  text_layer_destroy(nearby_layer);
 }
 
 static void setupMessaging(void) {
 	app_message_register_inbox_received(in_received_handler);
-	const uint32_t inbound_size = 64;
-	const uint32_t outbound_size = 64;
+	const uint32_t inbound_size = 128;
+	const uint32_t outbound_size = 128;
 	app_message_open(inbound_size, outbound_size);
 }
 
